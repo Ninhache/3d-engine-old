5f6d84667b0e57165a4794af75351d0ebe284d8f
Néo ALMEIDA <neo.almeida.etu@univ-lille.fr>
2023-01-25 09:09:02 +0100

    fix: modify data structure

--- src/headers/texture.h
+++ src/headers/texture.h
@@ -3,30 +3,26 @@
 
 #include <map>
 #include "glad/glad.h"
+#include <assimp/material.h>
 #include <stb/stb_image.h>
 #include <string>
 
 class Texture
 {
 	public:
-		static void loadTextureInMemory(std::string);
-		static Texture getTextureFromFile(std::string);
-		static void unloadTexture(std::string);
-
-		int getWidth() { return m_width; }
-		int getHeight() { return m_height; }
-		int getChannels() { return m_channels; }
+		static void loadTextureInMemory(std::string, aiTextureType);
+		static Texture getTextureFromFile(std::string, aiTextureType);
 
 		~Texture();
 
 	private:
 		GLuint m_texture;
-		int m_width, m_height, m_channels;
 		std::string m_filename;
-		static std::map<std::string, Texture*> m_map;
+		aiTextureType m_texture_type;
+		static std::map<std::string, Texture> m_map;
 
-		Texture(std::string);
-		
+		Texture(std::string, aiTextureType texture_type);
+
 };
 
 #endif
--- src/texture.cpp
+++ src/texture.cpp
@@ -1,14 +1,25 @@
 #include "headers/texture.h"
 #include "headers/logger.h"
 
-std::map<std::string, Texture*> Texture::m_map;
+std::map<std::string, Texture> Texture::m_map;
 
-Texture::Texture(std::string filename) {
+
+
+Texture::Texture(std::string filename, aiTextureType texture_type) {
     int width, height, channels;
     unsigned char* data = stbi_load(filename.c_str(), &width, &height, &channels, 0);
-    
+
     if (data == nullptr) {
         logger.log("Failed to load texture: " + filename);
+    }
+
+    GLenum channels_type;
+    if (channels == 1) {
+      channels_type = GL_RED;
+    } else if (channels == 3) {
+      channels_type = GL_RGB;
+    } else if (channels == 4) {
+      channels_type = GL_RGBA;
     }
 
     glGenTextures(1, &m_texture);
@@ -19,38 +30,29 @@ Texture::Texture(std::string filename) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-    this->m_width = width;
-    this->m_height = height;
-    this->m_channels = channels;
     this->m_filename = filename;
+    this->m_texture_type = texture_type;
 
     stbi_image_free(data);
 }
 
 Texture::~Texture() {
-    unloadTexture(m_filename);
     glDeleteTextures(1, &m_texture);
 }
 
-void Texture::loadTextureInMemory(std::string filename) {
-    Texture* texture = new Texture(filename);
-    Texture::m_map.insert(std::make_pair(filename, texture));
-}
-
-Texture Texture::getTextureFromFile(std::string filename) {
-    try {
-        return *Texture::m_map.at(filename);
-    } catch(const std::exception& e) {
-        loadTextureInMemory(filename);
+void Texture::loadTextureInMemory(std::string filename, aiTextureType texture_type) {
+    if (!m_map.count(filename)) {
+      Texture texture { filename, texture_type };
+      Texture::m_map.insert(std::make_pair(filename, texture));
     }
-
-    return *Texture::m_map.at(filename);
 }
 
-void Texture::unloadTexture(std::string filename) {
-    try {
-        Texture* texture = Texture::m_map.at(filename);
-        Texture::m_map.erase(filename);
-        free(texture);
-    } catch(const std::exception& e) { return; }
+Texture Texture::getTextureFromFile(std::string filename, aiTextureType texture_type) {
+  if (m_map.count(filename)) {
+    return Texture::m_map.at(filename);
+  } else {
+    loadTextureInMemory(filename, texture_type);
+  }
+
+  return Texture::m_map.at(filename);
 }
