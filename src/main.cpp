802c6c054dc06c0a77e55fbdab57465fb39141e2
Néo ALMEIDA <neo.almeida.etu@univ-lille.fr>
2023-02-15 10:40:02 +0100

    fix: misc.

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,7 @@
 
 int main(void) {
     
-    logger.setConsoleLog(true);
-    logger.log("Here's a logger..");
-
+    logger.log("Before render");
     Scene sc = Scene(800, 600);
     sc.renderLoop();
 
