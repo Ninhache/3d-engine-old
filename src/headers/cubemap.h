e084eac2390dee778f91c41c49ce34c8eab2d3df
Néo ALMEIDA <neo.almeida.etu@univ-lille.fr>
2023-03-08 11:24:45 +0100

    fix: move methods to .cpp

--- src/headers/cubemap.h
+++ src/headers/cubemap.h
@@ -1,3 +1,6 @@
+#ifndef CUBEMAP_HEADER_FILE
+#define CUBEMAP_HEADER_FILE
+
 #include <iostream>
 #include <vector>
 #include "texture.h"
@@ -20,95 +23,4 @@ private:
 	std::vector<float> m_vertices;
 };
 
-
-CubeMap::CubeMap(std::vector<std::string> paths) {
-	this->m_texture = Texture::loadCubemap(paths);
-	this->configureMesh();
-}
-
-CubeMap::CubeMap(std::string dir, std::vector<std::string> paths){
-	for(std::string& path : paths)
-	{
-		path = dir + "/" + path;
-	}
-	this->m_texture = Texture::loadCubemap(paths);
-	this->configureMesh();
-}
-
-void CubeMap::configureMesh() {
-    this->m_vertices = {
-    -1.0f,  1.0f, -1.0f,
-    -1.0f, -1.0f, -1.0f,
-     1.0f, -1.0f, -1.0f,
-     1.0f, -1.0f, -1.0f,
-     1.0f,  1.0f, -1.0f,
-    -1.0f,  1.0f, -1.0f,
-
-    -1.0f, -1.0f,  1.0f,
-    -1.0f, -1.0f, -1.0f,
-    -1.0f,  1.0f, -1.0f,
-    -1.0f,  1.0f, -1.0f,
-    -1.0f,  1.0f,  1.0f,
-    -1.0f, -1.0f,  1.0f,
-
-     1.0f, -1.0f, -1.0f,
-     1.0f, -1.0f,  1.0f,
-     1.0f,  1.0f,  1.0f,
-     1.0f,  1.0f,  1.0f,
-     1.0f,  1.0f, -1.0f,
-     1.0f, -1.0f, -1.0f,
-
-    -1.0f, -1.0f,  1.0f,
-    -1.0f,  1.0f,  1.0f,
-     1.0f,  1.0f,  1.0f,
-     1.0f,  1.0f,  1.0f,
-     1.0f, -1.0f,  1.0f,
-    -1.0f, -1.0f,  1.0f,
-
-    -1.0f,  1.0f, -1.0f,
-     1.0f,  1.0f, -1.0f,
-     1.0f,  1.0f,  1.0f,
-     1.0f,  1.0f,  1.0f,
-    -1.0f,  1.0f,  1.0f,
-    -1.0f,  1.0f, -1.0f,
-
-    -1.0f, -1.0f, -1.0f,
-    -1.0f, -1.0f,  1.0f,
-     1.0f, -1.0f, -1.0f,
-     1.0f, -1.0f, -1.0f,
-    -1.0f, -1.0f,  1.0f,
-     1.0f, -1.0f,  1.0f
-    };
-	this->configureBuffers();
-}
-
-void CubeMap::configureBuffers() {
-
-	glGenVertexArrays(1, &this->m_VAO);
-	glGenBuffers(1, &this->m_VBO);
-	
-	glBindVertexArray(this->m_VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, this->m_VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * m_vertices.size(), &this->m_vertices[0], GL_STATIC_DRAW);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
-
-	glBindVertexArray(0);
-}
-
-void CubeMap::draw(glm::mat4 view, glm::mat4 projection) {
-    
-    glDepthMask(false);
-
-    this->shader.use();
-    this->shader.setMatrix4("view", glm::mat4(glm::mat3(view)));
-    this->shader.setMatrix4("projection", projection);
-
-    glBindVertexArray(this->m_VAO);
-    glBindTexture(GL_TEXTURE_CUBE_MAP, this->m_texture.getID());
-    glDrawArrays(GL_TRIANGLES, 0, this->m_vertices.size());
-    
-    glDepthMask(true);
-}
+#endif // CUBEMAP_HEADER_FILE
