48259017b21d9e269ba6911eb3b5fbbba1178efb
Victor Mougel <victor.mougel.etu@univ-lille.fr>
2023-03-26 03:25:59 +0200

    commented cubemap and directionalLight.h

--- src/headers/cubemap.h
+++ src/headers/cubemap.h
@@ -12,17 +12,48 @@ class CubeMap
 
 public:
     /* Cubemaps in openGL use a Left handed coordinate system */
+	
+	/**
+	* @param paths - ALl path for the cubemap textures
+	* paths should look something like {"path/to/folder/posx.*"}
+	* for all values : "posx.jpg","negx.jpg","posy.jpg","negy.jpg","posz.jpg","negz.jpg" representing the faces of the cubemap
+	*/
 	CubeMap(std::vector<std::string> paths);
+	/**
+	* @param dir - The directory where the textures are located
+	* @param paths - All file names for the textures
+	* paths should look something like {"posx.*", "posy.*"} with dir being "path/to/folder/"
+	* for all values : "posx.jpg","negx.jpg","posy.jpg","negy.jpg","posz.jpg","negz.jpg" representing the faces of the cubemap
+	* 
+	* The constructor will concatenate all paths values with dir
+	*/
 	CubeMap(std::string dir, std::vector<std::string> paths);
+	/**
+	* Used for tests
+	*/
 	CubeMap(){
 		this->configureMesh();
 	}
+	/**
+	* sets m_vertices to make up a cube
+	*/
 	void configureMesh();
+	/**
+	* sets up VAO and VBO for the cube
+	*/
 	void configureBuffers();
+	/**
+	* @param view - The view matrix
+	* @param projecftion - The projection matrix
+	* Draw the cubemap (or skybox) to the screen
+	*/
     void draw(glm::mat4 view, glm::mat4 projection);
 protected:
+	//Texture of the cubemap containing all textures
 	Texture m_texture;
+	//The shader used to draw the cubemap
     std::unique_ptr<Shader> shader;
+	//Cube vertices
 	std::vector<float> m_vertices;
 private:
 	unsigned int m_VAO, m_VBO;
--- src/headers/directionalLight.h
+++ src/headers/directionalLight.h
@@ -9,18 +9,36 @@ class DirectionalLight : public Light
 {
 public:
 	DirectionalLight(glm::vec3 direction, glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f), float specular = 0.5f, float ambiant = 0.1f);
+	/**
+	* DirectionalLight does not have a mesh so this function is empty
+	*/
 	virtual void setupMesh();
+	/**
+	* @param shader - The shader used to draw the objects influenced by the light
+	* Sets the different properties of the light to the shader
+    * The properties are : color; specularStr; ambiantStr; active explained in light.h
+	*/
 	virtual void setUniforms(Shader& shader);
 	static int dirID;
 
+	/**
+	* Returns the name of the light, used for Imgui
+	*/
 	std::string getClassName() override {
 		return "Directional light";
 	};
 
+	/**
+	* Directional Light does not have a positionn, only a direction
+	*/
 	virtual const bool hasPosition() {
 		return false;
 	}
 
+	/**
+	* @param shader - The shader used to draw the objects influenced by the light
+	* Resets the light uniforms in the shader so that it does not affect objects anymore
+	*/
 	virtual void disableLight(Shader& shader);
 	virtual std::vector<Triple<std::string, std::string, float&>> getOptions();
 
